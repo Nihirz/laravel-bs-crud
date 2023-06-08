ac94f940b1ab415d441f46c2f48c53ed61b36334
Nihir Zala <zalanihir30@gmail.com>
2023-06-08 13:58:50 +0530

    Update the apis

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('api/index',[ApiController::class,'index']);
+Route::post('api/store',[ApiController::class,'store']);
+Route::post('api/edit/{id}',[ApiController::class,'edit']);
+Route::post('api/update',[ApiController::class,'update']);
+Route::post('api/destroy',[ApiController::class,'destroy']);
