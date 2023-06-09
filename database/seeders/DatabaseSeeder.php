ceb43102ada8c078cb3bf419b685696ea3a25c14
Nihir Zala <zalanihir30@gmail.com>
2023-06-09 14:32:26 +0530

    Added the relationship demo

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,6 +9,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            CategorySeeder::class,
             AppDatabaseSeeder::class,
             RolesDatabaseSeeder::class,
             SettingsDatabaseSeeder::class,
